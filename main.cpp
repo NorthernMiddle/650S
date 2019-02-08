bbd5aca384f0ee956aa7e97611dc0fd70c4ca2e6
Mr. Turner <43293575+nmsMentor@users.noreply.github.com>
2019-02-08 13:48:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,4 @@
-/**
- * main.cpp - main control program
- *
- * written for Northern Middle School Robotics Team 650S "Sea Bees"
- * V5 Competition Template
- * VRC 2018-19 "Turning Point"
- *  
- * @TODO: Finish out program
- * @TODO:
- */
-
 #include "robot-config.h"
-#include "pid.cpp"
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
 /*        Description: Competition template for VCS VEX V5                   */
@@ -21,21 +9,62 @@
 vex::competition    Competition;
 
 /*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*        Description: Program Variables                                     */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+//Creates variables to be used in program
+//arm speed variable
+int armSpeedPCT = 50;
+int autonDriveMotorSpeedPCT = 55;
+int autonPuncherMotorSpeedPCT = 200;
+//int spinnerSpeedPCT = 50;
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*        Description: User Functions                                        */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+void clearEncoders_func(){
+    //reset motor encoders to zero
+    FLmotor.resetRotation();
+    BLmotor.resetRotation();
+    FRmotor.resetRotation();
+    BRmotor.resetRotation();
+    }
+
+void autonSetMotorVelocity_func(){
+    //Set the velocity of the left and right motors to 55% power. 
+    //This command will not make the motor spin.
+    FLmotor.setVelocity(autonDriveMotorSpeedPCT, velocityUnits::pct);
+    BLmotor.setVelocity(autonDriveMotorSpeedPCT, velocityUnits::pct);
+    FRmotor.setVelocity(autonDriveMotorSpeedPCT, velocityUnits::pct);
+    BRmotor.setVelocity(autonDriveMotorSpeedPCT, velocityUnits::pct);
+    }
+
+void autonPuncher_func(){
+    if(Controller1.ButtonL1.pressing()){
+        Puncher.spin(vex::directionType::fwd,autonPuncherMotorSpeedPCT, vex::velocityUnits::pct);
+    }
+    else{Puncher.stop(vex::brakeType::brake);}
+    }
+
+/*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
 /*                                                                           */
 /*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.                                       */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the cortex has been powered on and    */
+/*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
-//static PIDControlled flywheel;
-
 void pre_auton( void ) {
-   // set Flywheel motor(s) encoder(s) to zero
-   Encoder.resetRotation(FlywheelLeft);
-
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-
+  clearEncoders_func();
 }
 
 /*---------------------------------------------------------------------------*/
@@ -49,35 +78,98 @@ void pre_auton( void ) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous( void ) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
+    autonSetMotorVelocity_func();
+    //---- RED TILE #1: R1 SHORT--SHOOT & PARK
+    
+    //Display that the program has started to the screen
+    Brain.Screen.print("AutonR1 SHORT--SHOOT & PARK HAS STARTED");
+    
+    //shoot ball 
+    
+    //go forward into flag
+    FLmotor.startRotateFor(4.2,rotationUnits::rev);
+    BLmotor.startRotateFor(4.2,rotationUnits::rev);
+    FRmotor.startRotateFor(-4.2,rotationUnits::rev);
+    BRmotor.startRotateFor(-4.2,rotationUnits::rev);
+    while(FLmotor.isSpinning()||BLmotor.isSpinning()||FRmotor.isSpinning()||BRmotor.isSpinning())
+    {}
+    
+    FLmotor.stop(brakeType::brake);
+    BLmotor.stop(brakeType::brake);
+    FRmotor.stop(brakeType::brake);
+    BRmotor.stop(brakeType::brake);
+    
+    vex::task::sleep(1000); 
+    
+    //back up to gray tile in front of red platform
+    FLmotor.startRotateFor(-5.6,rotationUnits::rev);
+    BLmotor.startRotateFor(-5.6,rotationUnits::rev);
+    FRmotor.startRotateFor(5.6,rotationUnits::rev);
+    BRmotor.startRotateFor(5.6,rotationUnits::rev);
+    while(FLmotor.isSpinning()||BLmotor.isSpinning()||FRmotor.isSpinning()||BRmotor.isSpinning())
+    {}
+    
+    FLmotor.stop(brakeType::brake);
+    BLmotor.stop(brakeType::brake);
+    FRmotor.stop(brakeType::brake);
+    BRmotor.stop(brakeType::brake); 
+    
+    vex::task::sleep(1000); 
+    
+    
+    //turn right
+    FLmotor.startRotateFor(-1.5,rotationUnits::rev);
+    BLmotor.startRotateFor(-1.5,rotationUnits::rev);
+    while(FLmotor.isSpinning()||BLmotor.isSpinning())
+    {}
+    
+    //go forward up on red platform
+    FLmotor.startRotateFor(-2,rotationUnits::rev);
+    BLmotor.startRotateFor(-2,rotationUnits::rev);
+    FRmotor.startRotateFor(-2,rotationUnits::rev);
+    BLmotor.startRotateFor(-2,rotationUnits::rev);
+    while(FLmotor.isSpinning()||BLmotor.isSpinning()||FRmotor.isSpinning()||BRmotor.isSpinning())
+    {}
+  }
 
-}
-
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*                              User Control Task                             */
+/*                                                                            */
+/*  This task is used to control your robot during the user control phase of  */
+/*  a VEX Competition.                                                        */
+/*                                                                            */
+/*  You must modify the code to add your own robot specific commands here.    */
+/*----------------------------------------------------------------------------*/
 
 void usercontrol( void ) {
   // User control code here, inside the loop
-  while (true) {
-    // This is the main execution loop for the user control program.
-    // Each time through the loop your program should update motor + servo
-    // values based on feedback from the joysticks.
+  while (1)
+  {
+    //Display that the program has started to the screen
+    Brain.Screen.print("User Control Has Started");
 
-    // ........................................................................
-    // Insert user code here. This is where you use the joystick values to
-    // update your motors, etc.
-    // ........................................................................
+    //Drive Controls
+    //Set the left and right motor to spin fwd/rev using the controller Axis as values as teh velocity value
+    BLmotor.spin(vex::directionType::rev, Controller1.Axis2.value(), vex::velocityUnits::pct);
+    BRmotor.spin(vex::directionType::fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
+    FLmotor.spin(vex::directionType::rev, Controller1.Axis2.value(), vex::velocityUnits::pct);
+    FRmotor.spin(vex::directionType::fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
 
-    vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources.
+    //arm motor commands
+    if(Controller1.ButtonR1.pressing())
+    {
+      Arm.spin(vex::directionType::rev, armSpeedPCT, vex::velocityUnits::pct);
+    }
+
+    else if(Controller1.ButtonR2.pressing())
+    {
+      Arm.spin(vex::directionType::fwd, armSpeedPCT, vex::velocityUnits::pct);
+    }
+    else
+    {
+      Arm.stop(vex::brakeType::brake);
+    }
   }
 }
 
@@ -85,10 +177,6 @@ void usercontrol( void ) {
 // Main will set up the competition functions and callbacks.
 //
 int main() {
-
-    //Use these variables to set the speed of the arm and claw
-    int armSpeedPCT = 50;
-    int clawSpeedPCT = 50;
 
     //Run the pre-autonomous function.
     pre_auton();
@@ -98,8 +186,7 @@ int main() {
     Competition.drivercontrol( usercontrol );
 
     //Prevent main from exiting with an infinite loop.
-    while(true) {
+    while(1) {
       vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
     }
-
 }
